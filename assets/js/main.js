6a9fa20be8bd02c09764b1bed640d6b9f38aef9c
nurafianZulkifli <63917225+nurafianZulkifli@users.noreply.github.com>
2022-06-03 18:15:07 +0800

    2022-06 Website Redesign Update to Version 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
-/**
-* Template Name: HeroBiz - v2.1.0
-* Template URL: https://bootstrapmade.com/herobiz-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+/** 
+ * Caught you snooping!
+ * I'm not that good in JS yet, so some codes were directly referenced to create the animations and transitions seen on the site.
+ **/
+
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
@@ -212,61 +211,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
   }
 
-  /**
-   * Clients Slider
-   */
-  new Swiper('.clients-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 40
-      },
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 60
-      },
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 80
-      },
-      992: {
-        slidesPerView: 6,
-        spaceBetween: 120
-      }
-    }
-  });
+  
 
   /**
-   * Testimonials Slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
-   * Testimonials Slider
+   * Portfolio Slider
    */
   new Swiper('.portfolio-details-slider', {
     speed: 600,
-    loop: true,
+    loop: false,
     autoplay: {
       delay: 5000,
       disableOnInteraction: false

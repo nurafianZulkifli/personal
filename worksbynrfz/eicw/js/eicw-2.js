f52a277847490a649f89abb43ed46319287d42bb
nrfz <menacingaskanois@gmail.com>
2025-02-01 18:43:36 +0800

    2025-02 Cumulative Update for Version 2501 (4.4.40)

--- worksbynrfz/eicw/js/eicw-2.js
+++ worksbynrfz/eicw/js/eicw-2.js
@@ -22,6 +22,7 @@ function updateHrefForDarkMode() {
     /* Banners */
     const coverSect = document.getElementById('cv-img');
     const iuSect = document.getElementById('iu-img');
+    const ieSect = document.getElementById('ie-img');
 
     /* Images */
     const lb1_link = document.getElementById('lb1');
@@ -48,6 +49,30 @@ function updateHrefForDarkMode() {
     const sne3_link = document.getElementById('sne3');
     const sne3_img = document.getElementById('sne3-img');
 
+    const evo2_link = document.getElementById('evo2');
+    const evo2_img = document.getElementById('evo2-img');
+
+    const ep1_link = document.getElementById('ep1');
+    const ep1_img = document.getElementById('ep1-img');
+
+    const ep2_link = document.getElementById('ep2');
+    const ep2_img = document.getElementById('ep2-img');
+
+    const ms1_link = document.getElementById('ms1');
+    const ms1_img = document.getElementById('ms1-img');
+
+    const ms2_link = document.getElementById('ms2');
+    const ms2_img = document.getElementById('ms2-img');
+
+    const ms3_link = document.getElementById('ms3');
+    const ms3_img = document.getElementById('ms3-img');
+
+    const ms4_link = document.getElementById('ms4');
+    const ms4_img = document.getElementById('ms4-img');
+
+    const ms5_link = document.getElementById('ms5');
+    const ms5_img = document.getElementById('ms5-img');
+
 
     /* Videos */
     const vid1 = document.getElementById('eicw2-vid1');
@@ -64,6 +89,7 @@ function updateHrefForDarkMode() {
         /* Banners */
         coverSect.style.backgroundImage = "url('../../img/eicw/cover-dark.png')";
         iuSect.style.backgroundImage = "url('../../img/eicw/iu-dark.png')";
+        ieSect.style.backgroundImage = "url('../../img/eicw/ie-dark.png')";
 
         /* Images */
         lb1_link.href = '../../img/eicw/lb1-dark.png';
@@ -90,6 +116,29 @@ function updateHrefForDarkMode() {
         sne3_link.href = '../../img/eicw/sne3-dark.png';
         sne3_img.src = '../../img/eicw/sne3-dark.png';
 
+        evo2_link.href = '../../img/eicw/evo2-dark.png';
+        evo2_img.src = '../../img/eicw/evo2-dark.png';
+
+        ep1_link.href = '../../img/eicw/ep1-dark.png';
+        ep1_img.src = '../../img/eicw/ep1-dark.png';
+
+        ep2_link.href = '../../img/eicw/ep2-dark.png';
+        ep2_img.src = '../../img/eicw/ep2-dark.png';
+
+        ms1_link.href = '../../img/eicw/ms1-dark.png';
+        ms1_img.src = '../../img/eicw/ms1-dark.png';
+
+        ms2_link.href = '../../img/eicw/ms2-dark.png';
+        ms2_img.src = '../../img/eicw/ms2-dark.png';
+
+        ms3_link.href = '../../img/eicw/ms3-dark.png';
+        ms3_img.src = '../../img/eicw/ms3-dark.png';
+
+        ms4_link.href = '../../img/eicw/ms4-dark.png';
+        ms4_img.src = '../../img/eicw/ms4-dark.png';
+
+        ms5_link.href = '../../img/eicw/ms5-dark.png';
+        ms5_img.src = '../../img/eicw/ms5-dark.png';
 
         /* Videos */
         vid1Source.src = '../../img/eicw/eicw2-vid1-dark.mp4';
@@ -99,6 +148,7 @@ function updateHrefForDarkMode() {
         /* Banners */
         coverSect.style.backgroundImage = "url('../../img/eicw/cover-light.png')";
         iuSect.style.backgroundImage = "url('../../img/eicw/iu-light.png')";
+        ieSect.style.backgroundImage = "url('../../img/eicw/ie-light.png')";
 
         /* Images */
         lb1_link.href = '../../img/eicw/lb1-light.png';
@@ -125,6 +175,29 @@ function updateHrefForDarkMode() {
         sne3_link.href = '../../img/eicw/sne3-light.png';
         sne3_img.src = '../../img/eicw/sne3-light.png';
 
+        evo2_link.href = '../../img/eicw/evo2-light.png';
+        evo2_img.src = '../../img/eicw/evo2-light.png';
+
+        ep1_link.href = '../../img/eicw/ep1-light.png';
+        ep1_img.src = '../../img/eicw/ep1-light.png';
+
+        ep2_link.href = '../../img/eicw/ep2-light.png';
+        ep2_img.src = '../../img/eicw/ep2-light.png';
+
+        ms1_link.href = '../../img/eicw/ms1-light.png';
+        ms1_img.src = '../../img/eicw/ms1-light.png';
+
+        ms2_link.href = '../../img/eicw/ms2-light.png';
+        ms2_img.src = '../../img/eicw/ms2-light.png';
+
+        ms3_link.href = '../../img/eicw/ms3-light.png';
+        ms3_img.src = '../../img/eicw/ms3-light.png';
+
+        ms4_link.href = '../../img/eicw/ms4-light.png';
+        ms4_img.src = '../../img/eicw/ms4-light.png';
+
+        ms5_link.href = '../../img/eicw/ms5-light.png';
+        ms5_img.src = '../../img/eicw/ms5-light.png';
 
         /* Videos */
         vid1Source.src = '../../img/eicw/eicw2-vid1-light.mp4';

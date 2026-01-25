de652874860b63b9964858b249c50fa408080db3
nrfz <menacingaskanois@gmail.com>
2026-01-25 22:00:33 +0800

    2026-02 Feature Update for Buszy V3

--- buszy/js/ann.js
+++ buszy/js/ann.js
@@ -17,16 +17,24 @@ async function fetchTrainServiceAlerts(retries = 7) {
 
             // Check if there is a valid alert message
             if (data && data.value && data.value.Message && Array.isArray(data.value.Message) && data.value.Message.length > 0) {
-                // Helper: convert URLs in text to clickable links
+                // Helper: convert URLs in text to clickable links and newlines to <br>
                 function linkify(text) {
-                    const urlRegex = /(https?:\/\/[\w\-._~:/?#[\]@!$&'()*+,;=%]+)|(www\.[\w\-._~:/?#[\]@!$&'()*+,;=%]+)/gi;
-                    return text.replace(urlRegex, function(url) {
+                    // Regex matches URLs (http, https, www) and go.gov.sg shortlinks
+                    const urlRegex = /(https?:\/\/[\w\-._~:/?#[\]@!$&'()*+,;=%]+)|(www\.[\w\-._~:/?#[\]@!$&'()*+,;=%]+)|(go\.gov\.sg\/[\w\-._~:/?#[\]@!$&'()*+,;=%]+)/gi;
+                    // Replace URLs with links
+                    let linked = text.replace(urlRegex, function(url) {
                         let href = url;
-                        if (!href.match(/^https?:\/\//i)) href = 'http://' + href;
+                        if (url.match(/^go\.gov\.sg\//i)) {
+                            href = 'https://' + url;
+                        } else if (!href.match(/^https?:\/\//i)) {
+                            href = 'http://' + href;
+                        }
                         return `<a href="${href}" target="_blank" rel="noopener noreferrer">${url}</a>`;
                     });
+                    // Replace newlines with <br>
+                    return linked.replace(/\n/g, '<br>');
                 }
-                const alerts = data.value.Message.map(msg => linkify(msg.Content || 'No content available')).join('<br>');
+                const alerts = data.value.Message.map(msg => linkify(msg.Content || 'No content available')).join('<br><br>');
                 alertBox.innerHTML = `<p><i class="fa-solid fa-bullhorn"></i> ${alerts}</p>`;
             } else {
                 alertBox.innerHTML = `<p><i class="fa-solid fa-bullhorn"></i> No Active Alerts.</p>`;
